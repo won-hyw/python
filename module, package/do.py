a11118c6b223cd57548ddb2b636cdd6e6e247afb
won-hyw <w2019@e-mirim.hs.kr>
2021-08-27 10:18:43 +0900

    package

--- module, package/do.py
+++ module, package/do.py
@@ -1,3 +1,25 @@
 import my_module as mm
 mm.지원이()
 print(mm.date)
+
+import travel.gangreung as ga
+gp = ga.GangreungPackage()
+print(gp)
+
+from travel import gangreung
+gp = gangreung.GangreungPackage()
+print(gp)
+
+from travel.gangreung import GangreungPackage
+gp = GangreungPackage()
+print(gp)
+
+from travel import ilsan
+ilsan.이지()
+
+import travel.ilsan
+travel.ilsan.이지()
+
+from travel.ilsan import 이지
+이지()
+
