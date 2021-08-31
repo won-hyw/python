22109552be81395ffac14b384b5b0c06ba781046
Ji-Saya <71277094+Ji-Saya@users.noreply.github.com>
2021-08-31 10:52:16 +0900

    package 2

--- module, package/do.py
+++ module, package/do.py
@@ -1,25 +1,29 @@
-import my_module as mm
-mm.지원이()
-print(mm.date)
+# import my_module as mm
+# mm.지원이()
+# print(mm.date)
+#
+# import travel.gangreung as ga
+# gp = ga.GangreungPackage()
+# print(gp)
+#
+# from travel import gangreung
+# gp = gangreung.GangreungPackage()
+# print(gp)
+#
+# from travel.gangreung import GangreungPackage
+# gp = GangreungPackage()
+# print(gp)
+#
+# from travel import ilsan
+# ilsan.이지()
+#
+# import travel.ilsan
+# travel.ilsan.이지()
+#
+# from travel.ilsan import 이지
+# 이지()
 
-import travel.gangreung as ga
-gp = ga.GangreungPackage()
-print(gp)
-
-from travel import gangreung
+from travel import *
+ilsan.이지()
 gp = gangreung.GangreungPackage()
 print(gp)
-
-from travel.gangreung import GangreungPackage
-gp = GangreungPackage()
-print(gp)
-
-from travel import ilsan
-ilsan.이지()
-
-import travel.ilsan
-travel.ilsan.이지()
-
-from travel.ilsan import 이지
-이지()
-
