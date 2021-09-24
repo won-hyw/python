108c8a4cd86eadcdcfcbfb21201aa78fc0359f7c
won-hyw <w2019@e-mirim.hs.kr>
2021-09-24 09:42:10 +0900

    예외 처리 없이 길이 오류 수정

--- module, package/baseball.py
+++ module, package/baseball.py
@@ -13,8 +13,8 @@ while True:
         continue
     # 길이가 3이 아닐 때 에러 처리
     if len(player) != 3:
-        raise InvalidCountError("3자리가 아닙니다.")
-
+        # raise InvalidCountError("3자리가 아닙니다.")
+        print(f'입력한 숫자의 개수가 정답과 다릅니다. 정답 : {len(answer)} 글자')
     # strike, ball 확인하자
     strike, ball = check(answer, player)
     # 출력하자
