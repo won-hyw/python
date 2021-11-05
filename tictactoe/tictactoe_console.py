6a1c7eaba422764f169eb3bac0decf4df106b3e9
won-hyw <80093106+won-hyw@users.noreply.github.com>
2021-11-05 09:58:21 +0900

    tictactoe_console 완성

--- tictactoe/tictactoe_console.py
+++ tictactoe/tictactoe_console.py
@@ -1,3 +1,6 @@
+from tictactoe.tictactoe_game_engine import TictactoeGameEngine
+
+
 class TictactoeConsole:
 
     def __init__(self):
@@ -5,10 +8,29 @@ class TictactoeConsole:
 
     def play(self):
         # show board
+        self.game_engine.show_board()
         # 무한반복
-        #   input row, col
-        #   set row, col
-        #   show board
-        #   set winner
-        #   승자가 있거나 무승부일 때, 게임 오버. 결과 출력하자
-        #   change turn
+        while True:
+            # input row, col
+            row = int(input('행 : '))
+            col = int(input('열 : '))
+            # set row, col
+            self.game_engine.set(row, col)
+            # show board
+            self.game_engine.show_board()
+            # set winner
+            winner = self.game_engine.set_winner()
+            # 승자가 있거나 무승부일 때, 게임 오버. 결과 출력하자
+            if winner == 'X' or winner == 'O':
+                print(f'{winner} 이김!! ㅊㅊ🎉')
+                break
+            elif winner == 'd':
+                print('무승부!!!')
+                break
+            # change turn
+            self.game_engine.change_turn()
+
+
+if __name__ == '__main__':
+    ttt_console = TictactoeConsole()
+    ttt_console.play()
